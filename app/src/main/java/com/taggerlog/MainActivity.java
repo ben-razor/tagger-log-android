3ec4e3975cd4094993fed03dcb009719c6850176
Ben Razor <chrisbenwar1@googlemail.com>
2021-01-25 12:05:13 +0000

    Change web view to read local web code instead of external url

--- app/src/main/java/com/taggerlog/MainActivity.java
+++ app/src/main/java/com/taggerlog/MainActivity.java
@@ -4,11 +4,13 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.JavascriptInterface;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
@@ -26,74 +28,22 @@ public class MainActivity extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private WebView webView;
-    private WebView windowWebView;
-    private ConstraintLayout container;
-    private String TAG = "taggerlog";
+    private String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mAuth = FirebaseAuth.getInstance();
+
         WebView.setWebContentsDebuggingEnabled(true);
         webView = (WebView) findViewById(R.id.webview);
-        windowWebView = (WebView) findViewById(R.id.newWindowWebView);
+
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webView.addJavascriptInterface(new TaggerLogInterface(this), "AndroidInterface");
         webSettings.setDomStorageEnabled(true);
-        webSettings.setSupportMultipleWindows(true);
-        webSettings.setCacheMode(WebSettings.LOAD_NO_CACHE);
-        webSettings.setUserAgentString("Mozilla/5.0 (Linux; Android 10; moto g(8) power) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36");
-        webView.setWebViewClient(new WebViewClient());
-        webView.setWebChromeClient(new WebChromeClient() {
-            @Override
-            public boolean onCreateWindow(WebView view, boolean isDialog, boolean isUserGesture,
-                                Message resultMsg) {
-                handleCreateWebWindowRequest(resultMsg);
-                return true;
-            }
-        });
-
-        webView.loadUrl("https://diarystore.firebaseapp.com/");
-    }
-
-    void handleCreateWebWindowRequest(Message resultMsg) {
-        if(resultMsg == null) {
-            return;
-        }
-        if(resultMsg.obj != null && resultMsg.obj instanceof WebView.WebViewTransport) {
-            WebView.WebViewTransport transport = (WebView.WebViewTransport)resultMsg.obj;
-
-            WebSettings windowWebViewSettings = windowWebView.getSettings();
-            windowWebViewSettings.setJavaScriptEnabled(true);
-            windowWebViewSettings.setJavaScriptCanOpenWindowsAutomatically(true);
-            windowWebViewSettings.setSupportMultipleWindows(true);
-            windowWebViewSettings.setDomStorageEnabled(true);
-            windowWebViewSettings.setUserAgentString("Mozilla/5.0 (Linux; Android 10; moto g(8) power) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Mobile Safari/537.36");
-            windowWebView.setWebViewClient(new WebViewClient());
-            windowWebView.setWebChromeClient(new WebChromeClient() {
-                @Override
-                public void onCloseWindow(WebView window) {
-                    super.onCloseWindow(window);
-                    handleCloseWebWindowRequest();
-                }
-            });
-
-            webView.setVisibility(View.GONE);
-            windowWebView.setVisibility(View.VISIBLE);
-            transport.setWebView(windowWebView);
-            resultMsg.sendToTarget();
-        }
-    }
-
-    void handleCloseWebWindowRequest() {
-        if (!isWebWindowOpened()) return;
-        windowWebView.setVisibility(View.INVISIBLE);
-        webView.setVisibility(View.VISIBLE);
-    }
-
-    boolean isWebWindowOpened() {
-        return true;
+        webView.loadUrl("file:///android_asset/web/index.html");
     }
 
     @Override
@@ -105,5 +55,27 @@ public class MainActivity extends AppCompatActivity {
 
     public void updateUI(FirebaseUser currentUser) {
 
+    }
+
+    /**
+     * Javascript interface for use by the WebView
+    */
+    public class TaggerLogInterface {
+        Context mContext;
+
+        /** Instantiate the interface and set the context */
+        TaggerLogInterface(Context c) {
+            mContext = c;
+        }
+
+        @JavascriptInterface
+        public void handleFloat(String control, float val) {
+            Log.i(control + " changed: ", Float.toString(val));
+
+            if(control.equals("pm-control-downers")) {
+            }
+            else if(control.equals("pm-control-uppers")) {
+            }
+        }
     }
 }
